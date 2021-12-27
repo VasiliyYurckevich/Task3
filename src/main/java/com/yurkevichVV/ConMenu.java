b8ce661ab2485b6dda2eee22ceb13f7f838481d7
VasiliyYurckevich <newskill228@gmail.com>
2021-12-27 14:52:06 +0300

    1.1

--- src/main/java/com/yurkevichVV/ConMenu.java
+++ src/main/java/com/yurkevichVV/ConMenu.java
@@ -9,9 +9,3 @@ public class ConMenu {
         System.out.println("0 - exit");
     }
 }
-    /*String[][] table = new String[][] { {moves[1], moves[2], moves[3],moves[4],moves[5],moves[6] },
-            {moves[1], "Draw", "Win", "Win","Lose","Lose" },
-            {moves[2], "Draw", "Win", "Win","Lose","Lose" },
-            {moves[3], "Draw", "Win", "Win","Lose","Lose" },
-            {moves[4], "Draw", "Win", "Win","Lose","Lose" },
-            {moves[5], "Draw", "Win", "Win","Lose","Lose" } };*/
